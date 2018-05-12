4d1d788118cc8dcf1a0fd520b4997962375d4db7
thedarkstrix <thedarkstrix@gmail.com>
2018-05-12 10:25:47 +0000

    kenzo: Add vndk hal

--- device.mk
+++ device.mk
@@ -40,6 +40,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.kenzo.rc
 
+#VNDK
+PRODUCT_PACKAGES += \
+    vndk-sp
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
